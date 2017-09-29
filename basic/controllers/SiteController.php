d35cfaf0edff9f8bb3971222d1054af86e98991e
codelove <626973633@qq.com>
2017-09-29 12:50:42 +0800

    update

--- basic/controllers/SiteController.php
+++ basic/controllers/SiteController.php
@@ -84,8 +84,8 @@ class SiteController extends BaseController
         $res['extend']['NO_REC'] = '0';
         $res['is_end'] = true;
         // $res['sequence'] = $post['sequence'];
-        $res['repeat_directive']['type'] = '1';
-        $res['repeat_directive']['content'] = '听不懂你在说什么';
+        // $res['repeat_directive']['type'] = '1';
+        // $res['repeat_directive']['content'] = '听不懂你在说什么';
         $res['timestamp'] = time();
         $res['versionid'] = '1.0';
         echo json_encode($res);
