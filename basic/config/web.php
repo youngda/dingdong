d48b2e330b281196ee2213b4da1bda9d5a62b35d
codelover <626973633@qq.com>
2017-12-04 22:24:54 +0800

    配置json

--- basic/config/web.php
+++ basic/config/web.php
@@ -11,6 +11,10 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'dingdongcodelover',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+                'text/json' => 'yii\web\JsonParser',
+            ]
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
